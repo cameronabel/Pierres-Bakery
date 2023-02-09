af83b2ca6aaccd6fa450cc1bf80534d88f5e1959
Cameron Abel <cameronabel@gmail.com>
2023-02-09 08:52:09 -0800

    boilerplate directory setup

---
+++ Bakery.Tests/ModelTests/Bakery.Tests.cs
@@ -0,0 +1,13 @@
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Bakery.Models;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class BakeryTests
+  {
+
+  }
+}
---
+++ Bakery/Program.cs
@@ -0,0 +1,9 @@
+using Bakery.Models;
+
+namespace Bakery
+{
+  public class Program
+  {
+
+  }
+}
