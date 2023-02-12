425e98205cf565970e64290bc37833f72733ad20
Cameron Abel <cameronabel@gmail.com>
2023-02-11 16:58:20 -0800

    Receipt pricing passes

--- Bakery/Models/Order.cs
+++ Bakery/Models/Order.cs
@@ -22,7 +22,7 @@ namespace Bakery.Models
       string receipt = "";
       foreach (Good good in Cart)
       {
-        receipt += $"    {good.Label,32}            \n";
+        receipt += $"    {good.Label,32}    {good.Price,4}    \n";
       }
       return receipt;
     }
